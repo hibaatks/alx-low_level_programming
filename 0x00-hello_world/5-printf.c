9bb6f26cb71110d17b5f33f5308e3b509e90963d
hibaatks <hibaaitkessou@gmail.com>
2023-10-05 17:35:09 -0700

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "with proper grammar, but the outcome is a piece of art,"
+#include <stdio.h>
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
