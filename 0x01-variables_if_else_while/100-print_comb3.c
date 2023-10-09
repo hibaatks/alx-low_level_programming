61889ccb5c8a156f4696a7d1fdf4c2026a721435
hibaatks <hibaaitkessou@gmail.com>
2023-10-09 11:38:30 -0700

    0x00-hello_world

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int first_digit, second_digit;
+
 	for (first_digit = 0; first_digit <= 8; first_digit++)
 		{
 			for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
