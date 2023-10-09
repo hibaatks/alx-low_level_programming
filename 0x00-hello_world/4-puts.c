0fa8cfbdd08a0defe88dab722b461886f2c30893
hibaatks <hibaaitkessou@gmail.com>
2023-10-09 10:57:56 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
+/bin/bash
 #include<stdio.h>
 /**
   * main-Entry point
   *
   * Return: Always 0 (Success)
   */
-Int main(void)
+int main(void)
 {
-Puts("\" Programming is like building a multilingual puzzle") ;
+puts("\" Programming is like building a multilingual puzzle") ;
 return (0);
 }
