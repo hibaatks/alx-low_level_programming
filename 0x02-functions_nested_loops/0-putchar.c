22827e9afa387aabcb10914e95c3bd3140ad85b7
hibaatks <hibaaitkessou@gmail.com>
2023-10-12 15:15:44 -0700

    0x00-hello_world

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,13 @@
-#include <stdio.h>
+/* 0-putchar.c */
+#include "main.h"
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-/* main.c */
-int _putchar(char c);
-
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
     _putchar('\n');
-
-    return 0;
+    return (0);
 }
