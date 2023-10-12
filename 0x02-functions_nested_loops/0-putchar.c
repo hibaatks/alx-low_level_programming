8e0b0e37d60269098c66bcb1bc85356e5e902e01
hibaatks <hibaaitkessou@gmail.com>
2023-10-12 15:25:17 -0700

    0x00-hello_world

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,33 @@
-/* 0-putchar.c */
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * _putchar - writes a character to the standard output
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
  */
 int main(void)
 {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
     _putchar('\n');
+
     return (0);
 }
