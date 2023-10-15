a463c04502e1b537a67beba6e336b731de0293a7
hibaatks <hibaaitkessou@gmail.com>
2023-10-15 15:08:36 -0700

    0x00-hello_world

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@
 void print_to_98(int n)
 {
 	int count;
-	
+
 	if (n <= 98)
 	{
 		for (count = n; count <= 98; count++)
 		{
 			printf("%d", count);
-			
+
 			if (count < 98)
 			{
 				printf(", ");
