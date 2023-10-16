0377539b08da8b7e7f118c2149fcfe77b128811b
hibaatks <hibaaitkessou@gmail.com>
2023-10-16 13:36:37 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -11,8 +10,8 @@ void print_numbers(void)
         char c;
 
         for (c = '0'; c <= '9'; c++)
-        {
-                _putchar(c);
-        }
-        _putchar('\n');
+{
+	 _putchar(c);
+}
+	_putchar('\n');
 }
